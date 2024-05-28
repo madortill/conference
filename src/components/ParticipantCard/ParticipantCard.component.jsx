cbdc1e676c2d14abeb345ac29f6fc0fe18a3e984
madortill <mador.till@gmail.com>
2024-05-28 09:53:13 +0300

    trying again

--- src/components/ParticipantCard/ParticipantCard.component.jsx
+++ src/components/ParticipantCard/ParticipantCard.component.jsx
@@ -1,7 +1,5 @@
 import './ParticipantCard.styles.css'
 import close from "../../media/images/close.png";
-import link from "../../media/images/external-link.png";
-import facebook from "../../media/images/facebook.png";
 
 const ParticipantCard = (props) => {
     console.log( props.infoObj);
@@ -13,14 +11,6 @@ const ParticipantCard = (props) => {
                 <h1 className='participant-card-topic not-press'>{props.infoObj.topic}</h1>
                 <h3 className='participant-card-name not-press'>{props.infoObj.name}</h3>
                 <p className='participant-card-text not-press'>{props.infoObj["full-text"]}</p>
-                <div className='link-icons-container'>
-                    { props.infoObj.website === undefined ? null : <a className='link-icon pointer' href={props.infoObj.website}>
-                        <img src={link} className='icon'/>
-                    </a>}
-                    { props.infoObj.facebook === undefined ? null : <a className='link-icon pointer' href={props.infoObj.facebook}>
-                        <img src={facebook} className='icon'/>
-                    </a>}
-                </div>
             </div>
         </div>
 
