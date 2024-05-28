fc3140b74758a1d24739912b3d0459b4a06a35ad
madortill <mador.till@gmail.com>
2024-05-28 15:39:17 +0300

    fixed contact

--- src/components/ContactInfo/ContactInfo.component.jsx
+++ src/components/ContactInfo/ContactInfo.component.jsx
@@ -6,8 +6,11 @@ const ContactInfo = () => {
     <div className="contact-info">
       {/* <h1 className='contact-header'>צור קשר</h1> */}
       <div className='black-contact'>
-        <h3 className='white-text contact-txt'>מוזמנים לפנות אלינו במייל:</h3>
-        <a className='white-text mail' href="mailto:evaluation.confe@gmail.com">evaluation.confe@gmail.com</a>
+        <div className='quote'>"התשובה בעשייה״</div>
+        <div className='flex-container'>
+          <h3 className='white-text contact-txt'>מוזמנים לפנות אלינו במייל:</h3>
+          <a className='white-text mail' href="mailto:evaluation.confe@gmail.com">evaluation.confe@gmail.com</a>
+        </div>
       </div>
     </div>
   );
