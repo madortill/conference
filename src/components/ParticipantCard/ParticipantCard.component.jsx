449fcfb62b3aace0dabedd6f96fa433aa6dff03f
madortill <mador.till@gmail.com>
2024-05-28 17:30:19 +0300

    did two pictuersss

--- src/components/ParticipantCard/ParticipantCard.component.jsx
+++ src/components/ParticipantCard/ParticipantCard.component.jsx
@@ -2,12 +2,20 @@ import './ParticipantCard.styles.css'
 import close from "../../media/images/close.png";
 
 const ParticipantCard = (props) => {
-    console.log( props.infoObj);
+//     const allPics = props.infoObj.insideImg.map((picture) =>
+//         <img key={picture} src={picture} className='main-img participant-card-img not-press'></img>
+// )
+
+// console.log(allPics)
     return (
         <div className='participant-card-container' onClick={(e) => e.target.classList.contains("not-press") ? null : props.closeReadMore()}>
             <div className='participant-card not-press'>
                 <div style={{ backgroundImage: `url(${close})` }} className='card-cose-btn pointer'></div>
-                <img src={props.infoObj.img} className='main-img participant-card-img not-press'></img>
+                <div className='pictures-container'>
+                    {props.infoObj.insideImg.map ((picture) => (
+                        <img src={picture} key={picture} className='main-img participant-card-img not-press' />
+                    ))}
+                </div>
                 <h1 className='participant-card-topic not-press'>{props.infoObj.topic}</h1>
                 <h3 className='participant-card-name not-press'>{props.infoObj.name}</h3>
                 <p className='participant-card-text not-press'>{props.infoObj["full-text"]}</p>
