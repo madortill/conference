c2bfd0fba579c3bddcae8d3a3db414d7927a89c7
madortill <mador.till@gmail.com>
2024-05-29 15:22:08 +0300

    done registration

--- src/components/Registration/Registration.component.jsx
+++ src/components/Registration/Registration.component.jsx
@@ -24,47 +24,45 @@ const Registration = () => {
         `רחלי גנשאפט פינטו | ארגון מע/אריך`,
     ]
 
-    const onFinish = useCallback(
-        async ({ email, name, level, phone, seat1, seat2, unit, role, bahad, car, number, color, model, food}) => {
+    const onFinish = useCallback (
+        async ({email, name, level, phone, seat1, seat2, unit, role, bahad, car, carNumber, color, model, food}) => {
             try {
-                const response = await fetch(
-                    'https://docs.google.com/forms/d/e/1FAIpQLSdMtLvbtHrp7MyuVdkbna_wTaUYvZBgXH4FAJrPYxHXkFgMKQ/formResponse?' +
-                    new URLSearchParams({
-                        'entry.343054181': name ?? '',
-                        'entry.1818583619': level ?? '',
-                        'entry.848216944': phone ?? '',
-                        'entry.525075253': seat1,
-                        'entry.1337870525': seat2,
-                        'entry.1931152825': unit ?? '',
-                        'entry.1700613233': role ?? '',
-                        'entry.94308182': bahad,
-                        'entry.2065539728': car,
-                        'entry.990947724': number ?? '',
-                        'entry.430422737': color ?? '',
-                        'entry.1975254107': model ?? '',
-                        'entry.1088207776': food,
+                const response = await fetch (
+                    "https://docs.google.com/forms/d/e/1FAIpQLSeP_JCei3YiwtjsZUfP8czx47v61DLXqWsofz4dZAh6x8RUXQ/formResponse?" +
+                    new URLSearchParams ({
+                        'entry.861887433': name ?? '',
+                        'entry.1522919113': level ?? '',
+                        'entry.2126579341': phone ?? '',
+                        'entry.896602696': seat1,
+                        'entry.1817269316': seat2,
+                        'entry.995594023': unit ?? '',
+                        'entry.1774602902': role ?? '',
+                        'entry.1658389145': bahad,
+                        'entry.1196107794': car,
+                        'entry.121284304': carNumber ?? '',
+                        'entry.237027329': color ?? '',
+                        'entry.932918356': model ?? '',
+                        'entry.1365822488': food,
                         emailAddress: email,
                     }),
                     {
                         mode: 'no-cors',
                     }
+
+        );
+            api.success({
+                    message: 'Submitted successfully',
+                },
+                    setIsConfirmed(true),
+                    setIsSubmited(true)
                 );
-// Check for status code 200 (success)
-                // console.log(response);
-                // if (response.status >= 200 && response.status < 400) {
-                    api.success({
-                        message: 'Submitted successfully',
-                    },
-                        setIsConfirmed(true),
-                        setIsSubmited(true)
-                    );
                     form.resetFields();
                     // } else {
                     //     api.error({
                     //         message: 'error',
                     //     });
                     // }
-                    console.log(response.redirected)
+                    console.log(response)
                 } catch (e) {
                     // api.open({
                     //     message: 'Error Occured',
@@ -83,7 +81,196 @@ const Registration = () => {
 
     return (
         <>
-            {isSubmited ? <Confirmation></Confirmation> : null}
+        {contextHolder}
+        <div className='form-container'>
+            <Form
+                form={form}
+                onFinish={onFinish}
+                layout="vertical"
+            >
+                <Form.Item
+                    className='form-item'
+                    name="email"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>email</label>}
+                    rules={[{ required: true, message: 'אנא הכניסו את המייל שלכם.' }]}
+                >
+                    <Input className='form-input' />
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="name"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>שם מלא</label>}
+                    rules={[{ required: true, message: 'אנא הכניסו את השם שלכם.' }]}
+                >
+                    <Input className='form-input' />
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="level"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>דרגה (לאנשי צבא בלבד)</label>}
+                    rules={[{ required: false, message: 'אנא הכניסו את דרגה שלכם.' }]}
+                >
+                    <Input className='form-input' />
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="phone"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>טלפון</label>}
+                    rules={[{ required: true, message: 'אנא הכניסו את טלפון שלכם.' }]}
+                >
+                    <Input className='form-input' />
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="seat1"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}> מבין האפשרויות הבאות, עלייך לבחור באפשרות המועדפת עלייך: מושב ראשון</label>}
+                    rules={[{ required: true, message: 'אנא בחרו מושב ראשון.' }]}
+                >
+                    <Radio.Group>
+                        <Space className='radio-container' direction="vertical">
+                            {SEAT1.map((name) => (
+                                <Radio key={name} className='radio-btn' value={name}>{name}</Radio>
+                            ))}
+                        </Space>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="seat2"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}> מבין האפשרויות הבאות, עלייך לבחור באפשרות המועדפת עלייך:  מושב שני</label>}
+                    rules={[{ required: true, message: 'אנא בחרו מושב שני.' }]}
+                >
+                    <Radio.Group>
+                        <Space className='radio-container' direction="vertical">
+                            {SEAT2.map((name) => (
+                                <Radio key={name} className='radio-btn' value={name}>{name}</Radio>
+                            ))}
+                        </Space>
+                    </Radio.Group>
+                </Form.Item>
+                <Form.Item
+                    className='form-item'
+                    name="unit"
+                    label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>ארגון  / יחידה</label>}
+                    rules={[{ required: true, message: 'אנא הכניסו את שם הארגון / היחידה שלכם.' }]}
+                >
+                    <Input className='form-input' />
+                </Form.Item>
+                <Form.Item
+                            className='form-item'
+                            name="role"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>תפקיד בארגון</label>}
+                            rules={[{ required: true, message: 'אנא הכניסו את תפקידכם בארגון.' }]}
+                        >
+                            <Input className='form-input' />
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item form-select'
+                            name="bahad"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>אם הינך מקריית ההדרכה: נא לבחור בבה״ד שלכם.</label>}
+                            rules={[{ required: false, message: "" }]}>
+                            <Select
+                                initialvalues={{
+                                    value: 'בחר',
+                                    label: 'בחר',
+                                }}
+
+                                options={[
+                                {
+                                    value: 'בה״ד 6',
+                                    label: 'בה״ד 6',
+                                },
+                                {
+                                    value: 'בה״ד 7',
+                                    label: 'בה״ד 7',
+                                },
+                                {
+                                    value: 'בה״ד 10',
+                                    label: 'בה״ד 10',
+                                },
+                                {
+                                    value: 'בה״ד 11',
+                                    label: 'בה״ד 11',
+                                },
+                                {
+                                    value: 'בה״ד 13',
+                                    label: 'בה״ד 13',
+                                },
+                                {
+                                    value: 'בה״ד 20',
+                                    label: 'בה״ד 20',
+                                },
+                                {
+                                    value: 'בה״ד החינוך',
+                                    label: 'בה״ד החינוך',
+                                },
+                                {
+                                    value: 'מפקדה',
+                                    label: 'מפקדה',
+                                }
+                                ]}>
+
+                            </Select>
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item'
+                            name="car"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>דרגת סרן ומעלה: מעוניינים באישור כניסה לרכב</label>}
+                            rules={[{ required: false, message: "" }]}
+                        >
+                            <Radio.Group>
+                                <Space className='radio-container' direction="vertical">
+                                    <Radio className='radio-btn' value="כן">כן</Radio>
+                                    <Radio className='radio-btn' value="לא">לא</Radio>
+                                </Space>
+                            </Radio.Group>
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item'
+                            name="carNumber"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>אם כן, יש למלא את הפרטים הבאים: מספר הרכב</label>}
+                            rules={[{ required: false, message: "" }]}
+                        >
+                            <Input className='form-input' />
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item'
+                            name="color"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>צבע הרכב</label>}
+                            rules={[{ required: false, message: "" }]}
+                        >
+                            <Input className='form-input' />
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item'
+                            name="model"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>דגם הרכב</label>}
+                            rules={[{ required: false, message: "" }]}
+                        >
+                            <Input className='form-input' />
+                        </Form.Item>
+                        <Form.Item
+                            className='form-item'
+                            name="food"
+                            label={<label style={{ fontSize: "1.7rem", fontFamily: 'assistant' }}>בקשות מזון מיוחדות</label>}
+                            rules={[{ required: true, message: 'האם יש לכם בקשות מזון מיוחדות?' }]}
+                        >
+                            <Radio.Group>
+                                <Space className='radio-container' direction="vertical">
+                                    <Radio className='radio-btn' value="אין">אין</Radio>
+                                    <Radio className='radio-btn' value="צמחוני">צמחוני</Radio>
+                                    <Radio className='radio-btn' value="טבעוני">טבעוני</Radio>
+                                    <Radio className='radio-btn' value="צליאקי">צליאקי</Radio>
+                                    <Radio className='radio-btn' value="אחר">אחר</Radio>
+                                </Space>
+                            </Radio.Group>
+                        </Form.Item>
+                <Button type="primary" htmlType="submit" className='pointer' id='submitBtn'>
+                    שליחה
+                </Button>
+            </Form>
+            </div>
+            {/* {isSubmited ? <Confirmation></Confirmation> : null}
             {contextHolder}
             <div className='form-container'>
 
@@ -284,7 +471,7 @@ const Registration = () => {
                         </Button>
                     </div>
                 </Form >
-            </div >
+            </div > */}
         </>
     )
 }
